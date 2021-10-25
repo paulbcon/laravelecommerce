2c4e89880e37d806a8e92b2703dbaa748c1f6aea
Paul Contemplacion <paulbcon@gmail.com>
2021-10-25 17:10:04 -0400

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=>'required|max:191',
+            'email'=>'required|email|max:191|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+
+        ]);
+
+        if ($validator->fails())
+        {
+            return response()->json([
+                'validation_errors' => $validator->errors(),
+            ]);
+        } else {
+            $user = User::create([
+                'name'=>$request->name,
+                'email' =>$request->email,
+                'password' =>$request->password
+            ]);
+
+            $user->createToken($user->email.'_Token')->plainTextToken;
+
+            return response()->json([
+                'status'=>200,
+                'username'=>$user->name,
+                'token'=>$token,
+                'message'=>'Registered Successfully',
+            ])
+        }
+    }
+}
