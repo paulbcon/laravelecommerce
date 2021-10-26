71bfc697fe845d90ec7a352ce566041cbf05a184
Paul Contemplacion <paulbcon@gmail.com>
2021-10-26 17:27:50 -0400

    Handle Auth User o redirect to admin dashboard or home page

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,21 @@ use App\Http\Controllers\API\AuthController;
 Route::post('register',[AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
+Route::middleware(['auth:sanctum','isAPIAdmin'])->group(function () {
+    Route::get('/checkingAuthenticated', function() {
+        return response()->json(['message' => 'You are in', 'status'=>200], 200);
+    });
+
+
+
+});
+
 Route::middleware(['auth:sanctum'])->group(function () {
+
     Route::post('logout',[AuthController::class, 'logout']);
+
 });
 
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
