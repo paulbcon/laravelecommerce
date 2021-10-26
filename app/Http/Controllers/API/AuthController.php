32680ede5c21adf8871624d9f229e465d44b7ca5
Paul Contemplacion <paulbcon@gmail.com>
2021-10-26 13:10:18 -0400

    Register and Login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\API;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
@@ -27,17 +29,50 @@ class AuthController extends Controller
             $user = User::create([
                 'name'=>$request->name,
                 'email' =>$request->email,
-                'password' =>$request->password
+                'password' =>Hash::make($request->password),
             ]);
 
-            $user->createToken($user->email.'_Token')->plainTextToken;
+            $token = $user->createToken($user->email.'_Token')->plainTextToken;
 
             return response()->json([
                 'status'=>200,
                 'username'=>$user->name,
                 'token'=>$token,
                 'message'=>'Registered Successfully',
-            ])
+            ]);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|max:191',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails())
+        {
+            return response()->json([
+                'validation_errors' => $validator->errors(),
+            ]);
+        } else {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Invalid Credentials',
+                ]);
+            } else {
+                $token = $user->createToken($user->email.'_Token')->plainTextToken;
+
+                return response()->json([
+                    'status'=>200,
+                    'username'=>$user->name,
+                    'token'=>$token,
+                    'message'=>'Logged In Successfully',
+                ]);
+            }
         }
     }
 }
